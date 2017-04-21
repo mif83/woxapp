d2064af4d87d5bb8688c0f06233de8634af5a4e7
Dima Esipenko <mif83@mail.ru>
2017-04-21 16:32:17 +0300

    something add

--- table/js/my.js
+++ table/js/my.js
@@ -1,3 +1,11 @@
 var app = angular.module("tableApp", []);
 
-console.log("Hi");
+app.directive('data', function () {
+    return {
+        restrict: 'EA',
+        templateUrl: "data-component.html",
+        link: function (scope, element, attrs) {
+            console.log('directive')
+        }
+    };
+});
