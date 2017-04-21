df8acba0da68759569c22016a20d828e359b2a42
Dima Esipenko <mif83@mail.ru>
2017-04-21 23:14:54 +0300

    add

--- table/js/my.js
+++ table/js/my.js
@@ -1,11 +1,26 @@
 var app = angular.module("tableApp", []);
 
+app.controller("myCtrl", function($scope, $http){
+    $scope.arr = [1];
+    $scope.myData = getData;
+    function getData(url){
+            $http.get("db/"+ url).then(function(res) {
+                $scope.items=res.data;
+            }).catch(function(err) {
+                alert("Ошибка загрузки данных", err);
+            });
+    }
+    $scope.render = function () {
+        console.log("add");
+        $scope.arr.push(Math.random());
+    }
+});
 app.directive('data', function () {
     return {
         restrict: 'EA',
         templateUrl: "data-component.html",
         link: function (scope, element, attrs) {
-            console.log('directive')
+
         }
     };
 });
