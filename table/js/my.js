4b2167a5db794179babbd7fb0cabea224ef94e5e
Dima Esipenko <mif83@mail.ru>
2017-04-22 20:06:35 +0300

    add sort

--- table/js/my.js
+++ table/js/my.js
@@ -10,12 +10,25 @@ app.controller("mainCtrl", function($scope){
 
 app.controller("myCtrl", function($scope, $http){
     var currentPage = 0,
-        itemsPerPage = 20;
-        dataList = [];
-
+        itemsPerPage = 20,
+        col,
+        dataList = [],
+        direction = {
+            0:true,
+            1:false,
+            2:false,
+            3:false,
+            current:0
+        };
+    $scope.isClick = false;
     $scope.myData = getData;
     $scope.showPage = showPage;
     $scope.getCurrentPageNum = getCurrentPageNum;
+    $scope.sort = sort;
+
+    $scope.sortType     = '1';
+    $scope.sortReverse  = false;
+    $scope.searchField   = '';
 
     function showPage(page) {
         $scope.currentDataList = getPageLists(page);
@@ -59,6 +72,52 @@ app.controller("myCtrl", function($scope, $http){
     }
     function getCurrentPageNum() {
         return currentPage;
+    }
+    function sort(column) {
+        col = column;
+        if (direction[col]){
+            dataList.sort(sortArrDown, col);
+        } else{
+            dataList.sort(sortArrUp, col);
+        }
+        direction[col] = !direction[col];
+        direction.current = col;
+        $scope.isClick = true;
+        $scope.direction = direction;
+        $scope.currentDataList = getPageLists();
+    }
+    function sortArrUp(a , b){
+        a = a[col];
+        b = b[col];
+        if (isNumeric(a)){
+            a = +a;
+            b = +b;
+        } else{
+            a = a.toLowerCase();
+            b = b.toLowerCase();
+        }
+        if(a > b) {
+            return +1;
+        }
+        return -1;
+    }
+    function sortArrDown(a ,b){
+        a = a[col];
+        b = b[col];
+        if (isNumeric(a)){
+            a = +a;
+            b = +b;
+        } else{
+            a = a.toLowerCase();
+            b = b.toLowerCase();
+        }
+        if(a < b) {
+            return +1;
+        }
+        return -1;
+    }
+    function isNumeric(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
     }
 });
 
