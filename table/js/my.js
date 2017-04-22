4188289f90a0863523c56e772909d0bde7bdda7e
Dima Esipenko <mif83@mail.ru>
2017-04-22 18:02:54 +0300

    add pagination

--- table/js/my.js
+++ table/js/my.js
@@ -1,20 +1,67 @@
 var app = angular.module("tableApp", []);
 
-app.controller("myCtrl", function($scope, $http){
-    $scope.arr = [1];
-    $scope.myData = getData;
-    function getData(url){
-            $http.get("db/"+ url).then(function(res) {
-                $scope.items=res.data;
-            }).catch(function(err) {
-                alert("Ошибка загрузки данных", err);
-            });
-    }
-    $scope.render = function () {
-        console.log("add");
-        $scope.arr.push(Math.random());
+app.controller("mainCtrl", function($scope){
+    $scope.instanse = [1];
+
+    $scope.createInstanse = function () {
+        $scope.instanse.push(Math.random());
     }
 });
+
+app.controller("myCtrl", function($scope, $http){
+    var currentPage = 0,
+        itemsPerPage = 20;
+        dataList = [];
+
+    $scope.myData = getData;
+    $scope.showPage = showPage;
+    $scope.getCurrentPageNum = getCurrentPageNum;
+
+    function showPage(page) {
+        $scope.currentDataList = getPageLists(page);
+    }
+    function getData(url){
+        $http.get("db/"+ url).then(function(res) {
+
+            dataList = res.data;
+            $scope.header = dataList[0];
+            dataList = dataList.slice(1);
+            $scope.currentDataList = getPageLists();
+        }).catch(function() {
+            alert("Ошибка загрузки данных");
+        });
+    };
+    function getPageLists(num){
+        var num = num || 0,
+            first = itemsPerPage * num,
+            last = first + itemsPerPage;
+        currentPage = num;
+
+        last = last > dataList.length? (dataList.length -1) : last;
+        $scope.paginationList = getPaginationList();
+        return dataList.slice(first, last);
+    }
+    function getTotalPageNum() {
+        return Math.ceil(dataList.length / itemsPerPage);
+    }
+    function getPaginationList() {
+        var pagesNum = getTotalPageNum(),
+            paginationList =[];
+        for(var i =0; i < pagesNum; i++){
+            paginationList.push({
+                pageText:i+1,
+                pageLink:i
+            });
+        }
+        if(pagesNum > 1) {
+            return paginationList;
+        }
+    }
+    function getCurrentPageNum() {
+        return currentPage;
+    }
+});
+
 app.directive('data', function () {
     return {
         restrict: 'EA',
